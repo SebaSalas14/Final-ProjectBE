b032dc882e13e5adc7177742c24e05a2502c61dc
Seba Salas <sebaasalas14@gmail.com>
2020-12-01 20:05:56 -0300

    add connectBD

--- config/db.js
+++ config/db.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: 'variables.env'});
+require('dotenv').config({ path: 'variables.env' });
 
-
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
+            useCreateIndex: false
+        })
+        console.log('DB Conectada')
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors= require('cors');
+const connectDB = require('./config/db');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+//conexión a la base de datos 
+connectDB();
 //Se Habilitan request desde cualquier URL
 app.use(cors());
 
